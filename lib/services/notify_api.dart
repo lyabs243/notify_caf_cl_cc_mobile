d0d64acb92346e3f565b05d9ea5de52d20519db2
lyabs243 <loicyabili@outlook.com>
2019-08-18 12:46:41 +0200

    add class to access to notifygroup api

---
+++ lib/services/notify_api.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class NotifyApi {
+
+  int statusCode;
+  Map mapResult;
+
+  Future<Map> getJsonFromServer(String url, Map params) async {
+
+    Map<String,String> map = {
+      'access_api': 'sgdhrnt_234lyS__',
+      'lang': 'en',
+      'version': '1',
+      'timezone': '+00:00'
+    };
+
+    if (params != null) {
+      map.addAll(params);
+    }
+
+    final response = await http.post(url,body: map);
+    this.statusCode = response.statusCode;
+    if (response.statusCode == 200) {
+      mapResult = json.decode(response.body);
+    }
+
+    print(mapResult['NOTIFYGROUP']);
+
+    return mapResult;
+  }
+
+}
