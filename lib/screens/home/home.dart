0fe8910a818ac80945b0a01b9b25f44fbf74aa8c
lyabs243 <loicyabili@outlook.com>
2019-08-19 22:38:22 +0200

    add user picture in home page

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'components/body.dart';
 import '../../models/localizations.dart';
 import '../login/login.dart';
+import '../../models/user.dart';
 
 class HomePage extends StatefulWidget{
 
@@ -19,6 +20,17 @@ class HomePage extends StatefulWidget{
 
 class _HomePageState extends State<HomePage>{
 
+  User user;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    User.getInstance().then((user){
+      this.user = user;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     Map localization = MyLocalizations.of(context).localization;
@@ -26,14 +38,28 @@ class _HomePageState extends State<HomePage>{
       appBar: AppBar(
         title: Text(localization['app_title']),
         actions: <Widget>[
-          new FlatButton(
-            onPressed: (){
-              Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){
-                return Login();
-              }));
-            },
-            child: Text(localization['login'],style: TextStyle(color: Colors.white),)
-          )
+          (user == null || user.id_accout_type == User.NOT_CONNECTED_ACCOUNT_ID) ?
+            new FlatButton(
+              onPressed: (){
+                Navigator.push(context, MaterialPageRoute(builder: (BuildContext context){
+                  return Login();
+                }));
+              },
+              child: Text(localization['login'],style: TextStyle(color: Colors.white),)
+            ) :
+            new Container(
+              width: 40,
+              height: 40,
+              margin: EdgeInsets.only(top: 9.0,bottom: 9.0, right: 5.0),
+              child: (user != null)?
+              CircleAvatar(
+                radius: 30.0,
+                backgroundImage:
+                NetworkImage(user.url_profil_pic),
+                backgroundColor: Colors.transparent,
+              ):
+              Icon(Icons.supervised_user_circle),
+            )
         ],
       ),
       body: Body(),
