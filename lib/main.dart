d9e3e5551747cbdb37a649ccc3365bda96fbd056
lyabs243 <loicyabili@outlook.com>
2019-08-26 14:44:14 +0200

    reset the facebook login settings

--- lib/main.dart
+++ lib/main.dart
@@ -31,12 +31,12 @@ class MyApp extends StatelessWidget {
           this.user = user;
         }),
         builder: (BuildContext context, AsyncSnapshot<User> snapshot){
-          /*if (user != null && user.id_accout_type != null && user.id_accout_type != null){
+          if (user != null && user.id_accout_type != null && user.id_accout_type != null){
             /// is because there is user already logged
             return HomePage();
-          }*/
+          }
           /// other way there is no user logged.
-          return Center();
+          return Login();
         }
       ),
     );
