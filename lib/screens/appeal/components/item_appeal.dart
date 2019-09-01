07d366aa064dc3cb837c28e8dec8ef80b896a4da
lyabs243 <loicyabili@outlook.com>
2019-09-01 14:35:03 +0200

    pull refresh page

--- lib/screens/appeal/components/item_appeal.dart
+++ lib/screens/appeal/components/item_appeal.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'appeal_dialog.dart';
 import '.././../../models/appeal_item.dart';
 import '../../../components/empty_data.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
 class ItemAppeal extends StatefulWidget{
 
@@ -23,96 +24,123 @@ class _ItemAppealState extends State<ItemAppeal>{
   Map localization;
   List<AppealItem> items;
   bool isPageLoading = true;
+  bool isPageRefresh = false;
+
+  RefreshController _refreshController;
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     localization = this.widget.localization;
+    _refreshController = RefreshController(initialRefresh: false);
     items = new List<AppealItem>();
-    AppealItem.getAppeals(page: 1).then((result){
-      setState(() {
-        items = result;
-        isPageLoading = false;
-      });
-    });
+    initItems();
   }
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return (isPageLoading)?
+    return SmartRefresher(
+      enablePullDown: true,
+      enablePullUp: true,
+      header: (WaterDropMaterialHeader(
+        backgroundColor: Theme.of(context).primaryColor,
+      )),
+      controller: _refreshController,
+      onRefresh: _onRefresh,
+      child: (isPageLoading)?
       Center(
         child: CircularProgressIndicator(),
       ):
+      (isPageRefresh)?
+      Center():
       (items.length<=0)?
-        EmptyData(localization):
-        ListView.builder(
-            itemCount: items.length,
-            itemBuilder: ((context,i){
-              return InkWell(
-                child: Card(
-                  child: Container(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            RichText(
-                              text: new TextSpan(
-                                text: items[i].full_name,
-                                style: new TextStyle(
-                                    color: Theme.of(context).textTheme.body1.color,fontSize: 20.0,
-                                    fontWeight: FontWeight.bold
-                                ),
-                                recognizer: new TapGestureRecognizer()
-                                  ..onTap = () {
+      EmptyData(localization):
+      ListView.builder(
+          itemCount: items.length,
+          itemBuilder: ((context,i){
+            return InkWell(
+              child: Card(
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: <Widget>[
+                          RichText(
+                            text: new TextSpan(
+                              text: items[i].full_name,
+                              style: new TextStyle(
+                                  color: Theme.of(context).textTheme.body1.color,fontSize: 20.0,
+                                  fontWeight: FontWeight.bold
+                              ),
+                              recognizer: new TapGestureRecognizer()
+                                ..onTap = () {
 
-                                  },
-                              ),
+                                },
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 20.0,
-                              margin: EdgeInsets.only(left: 5.0),
-                              decoration: BoxDecoration(
-                                  color: (items[0].is_policie_violate)? Colors.green : Colors.red,
-                                  shape: BoxShape.circle
-                              ),
+                          ),
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            margin: EdgeInsets.only(left: 5.0),
+                            decoration: BoxDecoration(
+                                color: (items[0].is_policie_violate)? Colors.green : Colors.red,
+                                shape: BoxShape.circle
                             ),
-                            Container(
-                              width: 20.0,
-                              height: 20.0,
-                              margin: EdgeInsets.only(left: 5.0),
-                              decoration: BoxDecoration(
-                                  color: (items[0].is_policie_respect_after_activation)? Colors.green : Colors.red,
-                                  shape: BoxShape.circle
-                              ),
+                          ),
+                          Container(
+                            width: 20.0,
+                            height: 20.0,
+                            margin: EdgeInsets.only(left: 5.0),
+                            decoration: BoxDecoration(
+                                color: (items[0].is_policie_respect_after_activation)? Colors.green : Colors.red,
+                                shape: BoxShape.circle
                             ),
-                          ],
-                        ),
-                        Padding(padding: EdgeInsets.only(bottom: 5.0),),
-                        Text(
-                          items[i].appeal_description,
-                          maxLines: 3,
-                        )
-                      ],
-                    ),
-                    margin: EdgeInsets.all(2.0),
-                    padding: EdgeInsets.only(left: 10.0,right: 10.0,bottom: 10.0),
+                          ),
+                        ],
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 5.0),),
+                      Text(
+                        items[i].appeal_description,
+                        maxLines: 3,
+                      )
+                    ],
                   ),
-                  elevation: 5.0,
-                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                  margin: EdgeInsets.all(2.0),
+                  padding: EdgeInsets.only(left: 10.0,right: 10.0,bottom: 10.0),
                 ),
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(
-                      builder: (BuildContext context){
-                        return new AppealDialog(this.localization);
-                      },
-                      fullscreenDialog: true
-                  ));
-                },
-              );
-            }));
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+              ),
+              onTap: (){
+                Navigator.of(context).push(MaterialPageRoute(
+                    builder: (BuildContext context){
+                      return new AppealDialog(this.localization);
+                    },
+                    fullscreenDialog: true
+                ));
+              },
+            );
+          })),
+    );
+  }
+
+  void _onRefresh() async{
+    await initItems();
+    _refreshController.refreshCompleted();
+  }
+
+  Future initItems() async{
+    setState(() {
+      isPageRefresh = true;
+      items.clear();
+    });
+    List<AppealItem> appeals = await AppealItem.getAppeals(page: 1);
+    setState(() {
+      items = appeals;
+      isPageRefresh = false;
+    });
   }
 }
