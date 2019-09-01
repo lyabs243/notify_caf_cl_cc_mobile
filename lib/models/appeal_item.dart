02d5de29565cf222ddc12e547c113a26f7597fc0
lyabs243 <loicyabili@outlook.com>
2019-09-01 13:39:53 +0200

    show appeal list items

--- lib/models/appeal_item.dart
+++ lib/models/appeal_item.dart
@@ -1,5 +1,6 @@
 import '../services/notify_api.dart';
 import 'user.dart';
+import 'package:intl/intl.dart';
 
 class AppealItem{
   
@@ -14,6 +15,7 @@ class AppealItem{
   bool active;
 
   static final String URL_ADD_APPEAL = 'http://notifygroup.org/notifyapp/api/index.php/subscriberAppeal/add/';
+  static final String URL_GET_APPEALS = 'http://notifygroup.org/notifyapp/api/index.php/subscriberAppeal/get_appeals/';
 
   AppealItem(this.id, this.id_subscriber, this.is_policie_violate,
       this.is_policie_respect_after_activation, this.full_name,
@@ -35,6 +37,35 @@ class AppealItem{
       }
     });
     return success;
+  }
+
+   static Future<List<AppealItem>> getAppeals({page: 1}) async{
+    List<AppealItem> items = new List<AppealItem>();
+    await NotifyApi().getJsonFromServer(URL_GET_APPEALS+page.toString(),null).then((map){
+      if(map != null && map['NOTIFYGROUP'][0]['success'] == 1.toString()) {
+        List data = map['NOTIFYGROUP'][0]['data'];
+        data.forEach((map){
+          int id = int.parse(map['id']);
+          int id_subscriber = int.parse(map['id_subscriber']);
+          bool is_policie_violate = (int.parse(map['is_policie_violate']) == 1)? true : false;
+          bool is_policie_respect_after_activation = (int.parse(map['is_policie_respect_after_activation']) == 1)? true : false;
+          String full_name = map['full_name'];
+          String appeal_description = map['appeal_description'];
+          bool approve = (int.parse(map['approve']) == 1)? true : false;
+
+          String format = 'yyyy-MM-dd H:mm:ss';
+          DateFormat formater = DateFormat(format);
+
+          DateTime register_date = formater.parse(map['register_date']);
+          bool active = (int.parse(map['active']) == 1)? true : false;
+
+          AppealItem item = new AppealItem(id, id_subscriber, is_policie_violate, is_policie_respect_after_activation,
+              full_name, appeal_description, approve, register_date, active);
+          items.add(item);
+        });
+      }
+    });
+    return items;
   }
 
 }
--- lib/screens/appeal/components/item_appeal.dart
+++ lib/screens/appeal/components/item_appeal.dart
@@ -1,83 +1,115 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'appeal_dialog.dart';
+import '.././../../models/appeal_item.dart';
 
-class ItemAppeal extends StatelessWidget{
+class ItemAppeal extends StatefulWidget{
 
   Map localization;
 
   ItemAppeal(this.localization);
 
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return ListView.builder(
-        itemCount: 5,
-        itemBuilder: ((context,i){
-          return InkWell(
-            child: Card(
-              child: Container(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        RichText(
-                          text: new TextSpan(
-                            text: 'Item $i',
-                            style: new TextStyle(
-                                color: Theme.of(context).textTheme.body1.color,fontSize: 20.0,
-                                fontWeight: FontWeight.bold
-                            ),
-                            recognizer: new TapGestureRecognizer()
-                              ..onTap = () {
-
-                              },
-                          ),
-                        ),
-                        Container(
-                          width: 20.0,
-                          height: 20.0,
-                          margin: EdgeInsets.only(left: 5.0),
-                          decoration: BoxDecoration(
-                              color: Colors.red,
-                              shape: BoxShape.circle
-                          ),
-                        ),
-                        Container(
-                          width: 20.0,
-                          height: 20.0,
-                          margin: EdgeInsets.only(left: 5.0),
-                          decoration: BoxDecoration(
-                              color: Colors.green,
-                              shape: BoxShape.circle
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(padding: EdgeInsets.only(bottom: 5.0),),
-                    Text(
-                      'Bla bla bla bal je viens chez toi pour blaguer ne menerve pas Bla bla bla bal je viens chez toi pour blaguer ne menerve pas Bla bla bla bal je viens chez toi pour blaguer ne menerve pas: $i',
-                      maxLines: 3,
-                    )
-                  ],
-                ),
-                margin: EdgeInsets.all(2.0),
-                padding: EdgeInsets.only(left: 10.0,right: 10.0,bottom: 10.0),
-              ),
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
-            ),
-            onTap: (){
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (BuildContext context){
-                    return new AppealDialog(this.localization);
-                  },
-                  fullscreenDialog: true
-              ));
-            },
-          );
-        }));
+  _ItemAppealState createState() {
+    // TODO: implement createState
+    return new _ItemAppealState();
   }
 
+}
+
+class _ItemAppealState extends State<ItemAppeal>{
+
+  Map localization;
+  List<AppealItem> items;
+  bool isPageLoading = true;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    localization = this.widget.localization;
+    items = new List<AppealItem>();
+    AppealItem.getAppeals(page: 1).then((result){
+      setState(() {
+        items = result;
+        isPageLoading = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return (isPageLoading)?
+        Center(
+          child: CircularProgressIndicator(),
+        ):
+        ListView.builder(
+            itemCount: items.length,
+            itemBuilder: ((context,i){
+              return InkWell(
+                child: Card(
+                  child: Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Row(
+                          children: <Widget>[
+                            RichText(
+                              text: new TextSpan(
+                                text: items[i].full_name,
+                                style: new TextStyle(
+                                    color: Theme.of(context).textTheme.body1.color,fontSize: 20.0,
+                                    fontWeight: FontWeight.bold
+                                ),
+                                recognizer: new TapGestureRecognizer()
+                                  ..onTap = () {
+
+                                  },
+                              ),
+                            ),
+                            Container(
+                              width: 20.0,
+                              height: 20.0,
+                              margin: EdgeInsets.only(left: 5.0),
+                              decoration: BoxDecoration(
+                                  color: (items[0].is_policie_violate)? Colors.green : Colors.red,
+                                  shape: BoxShape.circle
+                              ),
+                            ),
+                            Container(
+                              width: 20.0,
+                              height: 20.0,
+                              margin: EdgeInsets.only(left: 5.0),
+                              decoration: BoxDecoration(
+                                  color: (items[0].is_policie_respect_after_activation)? Colors.green : Colors.red,
+                                  shape: BoxShape.circle
+                              ),
+                            ),
+                          ],
+                        ),
+                        Padding(padding: EdgeInsets.only(bottom: 5.0),),
+                        Text(
+                          items[i].appeal_description,
+                          maxLines: 3,
+                        )
+                      ],
+                    ),
+                    margin: EdgeInsets.all(2.0),
+                    padding: EdgeInsets.only(left: 10.0,right: 10.0,bottom: 10.0),
+                  ),
+                  elevation: 5.0,
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+                ),
+                onTap: (){
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (BuildContext context){
+                        return new AppealDialog(this.localization);
+                      },
+                      fullscreenDialog: true
+                  ));
+                },
+              );
+            }));
+  }
 }
