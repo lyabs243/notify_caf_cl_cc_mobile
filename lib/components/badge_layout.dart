cca9fe54d6ab6d3d2a6a02119f9522cbf884a67a
lyabs243 <loicyabili@outlook.com>
2020-01-12 21:25:12 +0200

    Create fan badge layout

---
+++ lib/components/badge_layout.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_cafclcc/models/fan_badge.dart';
+import '../models/constants.dart' as constant;
+
+class BadgeLayout extends StatelessWidget {
+
+  Map localization;
+  FanBadge fanBadge;
+  double scale;
+
+  BadgeLayout(this.localization, this.fanBadge, {this.scale: 1.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform(
+        transform: new Matrix4.identity()..scale(scale),
+      child: Chip(
+      avatar: CircleAvatar(
+        backgroundColor: Colors.white,
+        child: Image.network(fanBadge.url_logo),
+      ),
+      backgroundColor: constant.fromHex(fanBadge.color),
+      label: Text(
+        fanBadge.title,
+        style: TextStyle(
+          color: Colors.white
+        ),
+      ),
+    )
+    );
+  }
+
+}
