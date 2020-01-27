8c83cd9e0a8493c0fc29966f903851cff2fb1561
lyabs243 <loicyabili@outlook.com>
2020-01-27 21:17:14 +0200

    Improve match details header layout

--- lib/screens/match_details/components/header.dart
+++ lib/screens/match_details/components/header.dart
@@ -33,124 +33,123 @@ class _HeaderState extends State<Header>{
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Column(
-      children: <Widget>[
-        Container(
+    return Container(
           color: Theme.of(context).primaryColor,
           width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height / 3.2,
           padding: EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              Text(
-                (matchItem.status != MatchItem.MATCH_STATUS_TYPE_PENDING)?
-                matchItem.match_status:
-                constant.formatDateTime(localization, matchItem.match_date, true),
-                style: TextStyle(
-                    color: Colors.white
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Text(
+                  (matchItem.status != MatchItem.MATCH_STATUS_TYPE_PENDING)?
+                  matchItem.match_status:
+                  constant.formatDateTime(localization, matchItem.match_date, true),
+                  style: TextStyle(
+                      color: Colors.white
+                  ),
                 ),
-              ),
-              Padding(padding: EdgeInsets.all(4.0),),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  mainAxisSize: MainAxisSize.max,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Column(
-                      children: <Widget>[
-                        Card(
-                          elevation: 10.0,
-                          child: Container(
-                            child: (matchItem.teamA_logo != null && matchItem.teamA_logo.length > 0)?
-                            Image.network(matchItem.teamA_logo,fit: BoxFit.cover,):
-                            Image.asset(
-                                'assets/icons/privacy.png'
+                Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Column(
+                        children: <Widget>[
+                          Card(
+                            elevation: 10.0,
+                            child: Container(
+                              child: (matchItem.teamA_logo != null && matchItem.teamA_logo.length > 0)?
+                              Image.network(matchItem.teamA_logo,fit: BoxFit.cover,):
+                              Image.asset(
+                                  'assets/icons/privacy.png'
+                              ),
+                              width: 100,
+                              height: 80,
                             ),
-                            width: 100,
-                            height: 80,
+                            color: Colors.transparent,
                           ),
-                          color: Colors.transparent,
-                        ),
-                        Padding(padding: EdgeInsets.all(4.0),),
-                        Container(
-                          child: Text(
-                            matchItem.teamA,
-                            textAlign: TextAlign.center,
-                            maxLines: 2,
-                            textScaleFactor: 1.5,
-                            style: TextStyle(
-                                color: Colors.white
+                          Padding(padding: EdgeInsets.all(4.0),),
+                          Container(
+                            child: Text(
+                              matchItem.teamA,
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              textScaleFactor: 1.5,
+                              style: TextStyle(
+                                  color: Colors.white
+                              ),
                             ),
+                            width: MediaQuery.of(context).size.width/3,
                           ),
-                          width: MediaQuery.of(context).size.width/3,
-                        ),
-                      ],
-                    ),
-                    Container(
-                      alignment: Alignment.center,
-                      height: MediaQuery.of(context).size.height/5,
-                      child: Text(
-                        (matchItem.status == MatchItem.MATCH_STATUS_TYPE_PENDING)?
-                        ' - ':
-                        (matchItem.teamA_goal.toString()+' - '+matchItem.teamB_goal.toString()),
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: Colors.white
-                        ),
-                        textScaleFactor: 2,
+                        ],
                       ),
-                      width: MediaQuery.of(context).size.width/6,
-                    ),
-                    Column(
-                      children: <Widget>[
-                        Card(
-                          elevation: 10.0,
-                          color: Colors.transparent,
-                          child: Container(
-                            child: (matchItem.teamB_logo != null && matchItem.teamB_logo.length > 0)?
-                            Image.network(matchItem.teamB_logo,fit: BoxFit.cover,):
-                            Image.asset(
-                                'assets/icons/privacy.png'
-                            ),
-                            width: 100,
-                            height: 80,
+                      Container(
+                        alignment: Alignment.center,
+                        height: MediaQuery.of(context).size.height/5,
+                        child: Text(
+                          (matchItem.status == MatchItem.MATCH_STATUS_TYPE_PENDING)?
+                          ' - ':
+                          (matchItem.teamA_goal.toString()+' - '+matchItem.teamB_goal.toString()),
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              color: Colors.white
                           ),
+                          textScaleFactor: 2,
                         ),
-                        Padding(padding: EdgeInsets.all(4.0),),
-                        Container(
-                          child: Text(
-                            matchItem.teamB,
-                            textAlign: TextAlign.center,
-                            maxLines: 2,
-                            textScaleFactor: 1.5,
-                            style: TextStyle(
-                                color: Colors.white
+                        width: MediaQuery.of(context).size.width/6,
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Card(
+                            elevation: 10.0,
+                            color: Colors.transparent,
+                            child: Container(
+                              child: (matchItem.teamB_logo != null && matchItem.teamB_logo.length > 0)?
+                              Image.network(matchItem.teamB_logo,fit: BoxFit.cover,):
+                              Image.asset(
+                                  'assets/icons/privacy.png'
+                              ),
+                              width: 100,
+                              height: 80,
                             ),
                           ),
-                          width: MediaQuery.of(context).size.width/3,
-                        ),
-                      ],
-                    ),
-                  ],
+                          Padding(padding: EdgeInsets.all(4.0),),
+                          Container(
+                            child: Text(
+                              matchItem.teamB,
+                              textAlign: TextAlign.center,
+                              maxLines: 2,
+                              textScaleFactor: 1.5,
+                              style: TextStyle(
+                                  color: Colors.white
+                              ),
+                            ),
+                            width: MediaQuery.of(context).size.width/3,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              (matchItem.team_a_penalty > 0 || matchItem.team_b_penalty > 0)?
-              Container(
-                child: Text(
-                    'PEN ${matchItem.team_a_penalty} - ${matchItem.team_b_penalty}',
-                    style: TextStyle(
-                        fontStyle: FontStyle.italic,
-                        color: Colors.white
-                    ),
-                    textAlign: TextAlign.center
-                ),
-                width: MediaQuery.of(context).size.width/4.5,
-              ): Container()
-            ],
+                (matchItem.team_a_penalty > 0 || matchItem.team_b_penalty > 0)?
+                Container(
+                  child: Text(
+                      'PEN ${matchItem.team_a_penalty} - ${matchItem.team_b_penalty}',
+                      style: TextStyle(
+                          fontStyle: FontStyle.italic,
+                          color: Colors.white
+                      ),
+                      textAlign: TextAlign.center
+                  ),
+                  width: MediaQuery.of(context).size.width/4.5,
+                ): Container()
+              ],
+            ),
           ),
-        )
-      ],
-    );
+        );
   }
 
 }
